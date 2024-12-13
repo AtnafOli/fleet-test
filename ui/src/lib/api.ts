75810134ee4e54244bbac23d7fef4cf57942d000
AtnafOli <atinafabebe2@gmail.com>
2024-12-13 15:04:30 +0300

    fix cors issue

--- ui/src/lib/api.ts
+++ ui/src/lib/api.ts
@@ -4,9 +4,11 @@ const API_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:8005/api";
 
 export const apiClient = axios.create({
   baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  timeout: 10000,
+  headers: { "Content-Type": "application/json" },
+
+  // Enable cross-domain requests
+  withCredentials: true,
 });
 
 export type ApiError = {
